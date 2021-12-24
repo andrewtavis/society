88fefe98898d194a2181e9c6040a13c909885d03
Andrew Tavis <andrew.t.mcallister@gmail.com>
2021-12-24 15:08:03 +0100

    Remove .pages files, card spellcheck and add docstrings

--- scripts/cards2pdf_png.py
+++ scripts/cards2pdf_png.py
@@ -1,9 +1,16 @@
+"""
+Generates PDFs and PNGs of cards from .txt files
+"""
+
 import os
 
 for fname in os.listdir("../resources/cards/generated"):
     if fname.endswith(".svg"):
         basename = fname[:-4]
-        for ext in ['pdf', 'png']:
-            os.system(("inkscape ../resources/cards/generated/{} " +
-                      "--export-filename=../resources/cards/generated/{}/{}.{}").format(fname, ext, basename, ext))
-
+        for ext in ["pdf", "png"]:
+            os.system(
+                (
+                    "inkscape ../resources/cards/generated/{} "
+                    + "--export-filename=../resources/cards/generated/{}/{}.{}"
+                ).format(fname, ext, basename, ext)
+            )
--- scripts/generate_cards.py
+++ scripts/generate_cards.py
@@ -1,6 +1,8 @@
+"""
+Generates SVGs of cards from .txt files
+"""
 
-
-for kind in ['event', 'action']:
+for kind in ["event", "action"]:
     with open("../resources/cards/{}_cards.txt".format(kind), "r") as txt:
         title = None
         for idx, line in enumerate(txt):
@@ -8,11 +10,17 @@ for kind in ['event', 'action']:
             if idx % 3 == 0:
                 if title:
                     with open("../templates/{}_cards.svg".format(kind), "r") as svg:
-                        with open("../resources/cards/generated/{}_{}.svg".format(kind, idx // 3), "w") as out:
+                        with open(
+                            "../resources/cards/generated/{}_{}.svg".format(
+                                kind, idx // 3
+                            ),
+                            "w",
+                        ) as out:
                             for l in svg:
-                                l = l.replace("{{title}}", title).replace("{{content}}", content)
+                                l = l.replace("{{title}}", title).replace(
+                                    "{{content}}", content
+                                )
                                 out.write(l)
                 title = line
             elif idx % 3 == 1:
                 content = line[1:]
-    
